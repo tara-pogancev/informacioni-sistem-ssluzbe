5209ef3b20124297a765b0f184aabc953c80ad23
jelenahrnjak <jelenahrnjak@uns.ac.rs>
2020-11-17 23:02:21 +0100

    #toolbar prva izmena Toolbar klase

--- src/gui/Toolbar.java
+++ src/gui/Toolbar.java
@@ -2,9 +2,11 @@
 
 package gui;
 
+import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
 
@@ -22,34 +24,39 @@ public class Toolbar extends JToolBar {
 		setFloatable(false);
 		
 		JButton addNew = new JButton();
-		addNew.setIcon(new ImageIcon("images/round_plus_icon16.png"));
-		addNew.setToolTipText("Add New");
+//		addNew.setIcon(new ImageIcon("images/round_plus_icon16.png"));
+		addNew.setIcon(new ImageIcon("images/add-icon.png"));
+		addNew.setToolTipText("New");
 		
 		add(addNew);
 		addSeparator();
 		
 		JButton edit = new JButton();
-		edit.setIcon(new ImageIcon("images/pencil_icon16.png"));
+//		edit.setIcon(new ImageIcon("images/pencil_icon16.png"));
+		edit.setIcon(new ImageIcon("images/edit-icon.png"));
 		edit.setToolTipText("Edit");
 		
 		add(edit);
 		addSeparator();
 		
 		JButton delete = new JButton();
-		delete.setIcon(new ImageIcon("images/trash_icon16.png"));
+//		delete.setIcon(new ImageIcon("images/trash_icon16.png"));
+		delete.setIcon(new ImageIcon("images/trash-icon.png"));
 		delete.setToolTipText("Delete");
 		
 		add(delete);
-		addSeparator();
 		
 		JButton search = new JButton();
-		search.setIcon(new ImageIcon("images/zoom_icon16.png"));
+//		search.setIcon(new ImageIcon("images/zoom_icon16.png"));
+		search.setIcon(new ImageIcon("images/find-icon.png"));
 		search.setToolTipText("Search");
 		
-		JTextArea search_text = new JTextArea("Search...");
-		search_text.setLineWrap(false);
 		
-		addSeparator();
+		JTextField search_text = new JTextField(15);
+		search_text.setMaximumSize(getPreferredSize());
+		search_text.setToolTipText("Search...");
+		
+		add(Box.createGlue());
 		add(search_text);
 		addSeparator();
 		add(search);
