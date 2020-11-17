9191d4b6e09bd14ea57c45f53f7fb27d57a91dda
jelenahrnjak <jelenahrnjak@uns.ac.rs>
2020-11-17 17:44:43 +0100

    #glavni_prozor inicijalna postavka MainFrame klase

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,6 +1,34 @@
 // #glavni-prozor
 package gui;
 
-public class MainFrame {
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+public class MainFrame extends JFrame{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8026416994513756565L;
+
+	public MainFrame() {
+		
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		
+		int windowHeight = (screenHeight /100) * 65;
+		int windowWidth = (screenWidth / 100) * 55;
+		
+		setSize(windowWidth, windowHeight);
+		setTitle("Studentska služba");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		
+		
+	}
 
 }
