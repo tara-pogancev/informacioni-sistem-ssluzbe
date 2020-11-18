d93ed0f082f39d845f7b999b92160b38d4050414
tara-pogancev <pogancev.ra80.2018@uns.ac.rs>
2020-11-18 19:51:03 +0100

    #menu_bar Implementiran glavni meni bar sa funkcionalnostima Help i About, kao i precicama i ikonicama.

---
+++ src/gui/HelpDialog.java
@@ -0,0 +1,206 @@
+// #menu_bar
+// Reference:
+// https://stackoverflow.com/questions/5165917/scrollable-html-jlabel
+// Model frame-a preuzet:
+// https://docs.oracle.com/javase/tutorial/uiswing/components/textfield.html
+//
+// https://www.baeldung.com/convert-file-to-input-stream
+// https://www.java-examples.com/create-custom-color-using-rgb-example
+
+package gui;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import javax.swing.*;
+import javax.swing.text.*;
+
+import javax.swing.event.*;
+import javax.swing.GroupLayout.*;
+
+public class HelpDialog extends JFrame implements DocumentListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3640861247170902722L;
+
+	private JTextField entry;
+	private JLabel text;
+	private JTextArea text_area;
+	private JLabel status;
+	private JScrollPane scroll_pane;
+	
+	
+	final static Color HILIT_COLOR = Color.LIGHT_GRAY;
+	final static Color ERROR_COLOR = new Color(237, 121, 121);
+	final static String CANCEL_ACTION = "cancel-action";
+	
+	final Color entryBg;
+	final Highlighter hilit;
+	final Highlighter.HighlightPainter painter;
+	
+	public HelpDialog() {
+
+		//Frame basics
+		this.setTitle("Help");
+		this.setResizable(false);
+		
+		entry = new JTextField();
+		text = new JLabel();
+		text_area = new JTextArea();
+		status = new JLabel();
+		
+		text_area.setColumns(20);
+		text_area.setLineWrap(true);
+		text_area.setRows(5);
+		text_area.setWrapStyleWord(true);
+		text_area.setEditable(false);
+		
+		scroll_pane = new JScrollPane(text_area);
+		
+		text.setText("Pretraži tekst:");
+		
+		GroupLayout layout = new GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		
+		ParallelGroup hgroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
+		
+		SequentialGroup h1 = layout.createSequentialGroup();
+		ParallelGroup h2 = layout.createParallelGroup(GroupLayout.Alignment.TRAILING);
+		
+		h1.addContainerGap();
+		
+		h2.addComponent(scroll_pane, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE);
+		h2.addComponent(status, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE);
+		
+		SequentialGroup h3 = layout.createSequentialGroup();
+		h3.addComponent(text);
+		h3.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED);
+		h3.addComponent(entry, GroupLayout.DEFAULT_SIZE, 321, Short.MAX_VALUE);
+		
+		h2.addGroup(h3);
+		h1.addGroup(h2);
+		h1.addContainerGap();
+		
+		hgroup.addGroup(GroupLayout.Alignment.TRAILING, h1);
+		layout.setHorizontalGroup(hgroup);
+		
+		ParallelGroup vgroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
+		SequentialGroup v1 = layout.createSequentialGroup();
+		v1.addContainerGap();
+		
+		ParallelGroup v2 = layout.createParallelGroup(GroupLayout.Alignment.BASELINE);
+		v2.addComponent(text);
+		v2.addComponent(entry, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE);
+		
+		v1.addGroup(v2);
+		v1.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+		v1.addComponent(scroll_pane, GroupLayout.DEFAULT_SIZE, 233, Short.MAX_VALUE);
+		v1.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+		v1.addComponent(status);
+		v1.addContainerGap();
+		
+		vgroup.addGroup(v1);
+		layout.setVerticalGroup(vgroup);
+		pack();
+		
+		//Inicijalizacija komponenti gotova
+	    File initialFile = new File("docs/help.txt");
+	    InputStream in = null;
+		try {
+			in = new FileInputStream(initialFile);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		
+        try {
+            text_area.read(new InputStreamReader(in), null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+		hilit = new DefaultHighlighter();
+		painter = new DefaultHighlighter.DefaultHighlightPainter(HILIT_COLOR);
+		text_area.setHighlighter(hilit);
+		
+		entryBg = entry.getBackground();
+		entry.getDocument().addDocumentListener(this);
+		
+		InputMap im = entry.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+		ActionMap am = entry.getActionMap();
+        im.put(KeyStroke.getKeyStroke("ESCAPE"), CANCEL_ACTION);
+        am.put(CANCEL_ACTION, new CancelAction());
+
+		this.setLocationRelativeTo(null);
+        
+	}
+
+	public void search() {
+		hilit.removeAllHighlights();
+
+		String s = entry.getText();
+		if (s.length() <= 0) {
+			message("Ništa za pretragu.");
+			return;
+		}
+
+		String content = text_area.getText();
+		int index = content.indexOf(s, 0);
+		if (index >= 0) { // match found
+			try {
+				int end = index + s.length();
+				hilit.addHighlight(index, end, painter);
+				text_area.setCaretPosition(end);
+				entry.setBackground(entryBg);
+				message("'" + s + "' pronaðen. Pritisni ESC da završiš pretragu.");
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			}
+		} else {
+			entry.setBackground(ERROR_COLOR);
+			message("'" + s + "' nije pronaðen. Pritisni ESC da zapoèneš novu pretragu.");
+		}
+
+	}
+	
+	public void message(String msg) {
+		status.setText(msg);
+	}
+	
+	
+	@Override
+	public void changedUpdate(DocumentEvent arg0) {	
+	}
+
+
+	@Override
+	public void insertUpdate(DocumentEvent arg0) {	
+		search();
+	}
+
+
+	@Override
+	public void removeUpdate(DocumentEvent arg0) {
+		search();
+	}
+	
+	class CancelAction extends AbstractAction {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 3407053550822654316L;
+
+		public void actionPerformed(ActionEvent ev) {
+			hilit.removeAllHighlights();
+			entry.setText("");
+			entry.setBackground(entryBg);
+		}
+	}
+	
+}
--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -1,10 +1,17 @@
 // #menu_bar
-
+// Reference:
+// Projekat Dogadjaji
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
 public class MainMenu extends JMenuBar {
 
@@ -16,6 +23,7 @@ public class MainMenu extends JMenuBar {
 
 	public MainMenu() {
 		
+		//Bazna konstrukcija
 		JMenu file = new JMenu("File");
 		JMenu edit = new JMenu("Edit");
 		JMenu help = new JMenu("Help");
@@ -29,6 +37,43 @@ public class MainMenu extends JMenuBar {
 		JMenuItem help_m = new JMenuItem("Help");
 		JMenuItem about_m = new JMenuItem("About");
 		
+		//Mnemonici, akceleratori, ikonice
+		file.setMnemonic(KeyEvent.VK_F);
+		edit.setMnemonic(KeyEvent.VK_E);
+		help.setMnemonic(KeyEvent.VK_H);
+		
+		new_m.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.ALT_MASK + ActionEvent.SHIFT_MASK));
+		close_m.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.ALT_MASK + ActionEvent.SHIFT_MASK));
+		edit_m.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+		delete_m.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, ActionEvent.CTRL_MASK));
+		help_m.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, ActionEvent.CTRL_MASK));
+		about_m.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.ALT_MASK));
+		
+		new_m.setIcon(new ImageIcon("images/add.png"));
+		close_m.setIcon(new ImageIcon("images/delete.png"));
+		edit_m.setIcon(new ImageIcon("images/edit.png"));
+		delete_m.setIcon(new ImageIcon("images/trash-icon.png"));
+		help_m.setIcon(new ImageIcon("images/alert.png"));
+		about_m.setIcon(new ImageIcon("images/information.png"));
+		
+		//Dodavanje funkcionalnosti
+		help_m.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				new HelpDialog().setVisible(true);
+			}
+		});
+		
+		about_m.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				new AboutDialog().setVisible(true);
+			}
+		});
+		
+		//Dodavanje elemenata
 		file.add(new_m);
 		file.addSeparator();
 		file.add(close_m);
@@ -40,8 +85,6 @@ public class MainMenu extends JMenuBar {
 		help.add(help_m);
 		help.addSeparator();
 		help.add(about_m);
-		
-		//TODO DODATI IKONICE I FUNKCIONALNOSTI
 		
 		add(file);
 		add(edit);
