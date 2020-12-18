8421dbc640f5453cf51b15cc7255355fb5d2cc67
jelenahrnjak <jelenahrnjak@uns.ac.rs>
2020-12-18 23:49:43 +0100

    #dodavanje_profesora Napravljen dijalog za dodavanje profesora(pocetna verzija)

--- src/listeners/action/AddNewAction.java
+++ src/listeners/action/AddNewAction.java
@@ -6,6 +6,7 @@ import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 import javax.swing.KeyStroke;
 
+import dialog.DodavanjeProfesora;
 import dialog.DodavanjeStudenta;
 
 public class AddNewAction extends AbstractAction {
@@ -36,7 +37,8 @@ public class AddNewAction extends AbstractAction {
 		 System.out.println("Action: Add New");
 		 
 		 //TODO Napraviti switch-case koji u zavisnosti od aktivnog taba prikaze tacno dodavanje, trenunto je ovako zbog testiranja
-		 new DodavanjeStudenta().setVisible(true);
+		 //new DodavanjeStudenta().setVisible(true);
+		 new DodavanjeProfesora().setVisible(true);
 		 
 	}
 
