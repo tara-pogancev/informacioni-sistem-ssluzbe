d962b37504320b277f242425e9ca5386104402f3
Tara Pogancev <pogancev.ra80.2018@uns.ac.rs>
2020-12-22 23:06:28 +0100

    #ocena Dodata klasa ocena

--- src/model/Ocena.java
+++ src/model/Ocena.java
@@ -1,5 +1,44 @@
+//#ocena
 package model;
 
 public class Ocena {
 
+	private Student student;
+	private Predmet predmet;
+	private int ocena;
+	private String datum;
+	
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	public Predmet getPredmet() {
+		return predmet;
+	}
+	public void setPredmet(Predmet predmet) {
+		this.predmet = predmet;
+	}
+	public int getOcena() {
+		return ocena;
+	}
+	public void setOcena(int ocena) {
+		this.ocena = ocena;
+	}
+	public String getDatum() {
+		return datum;
+	}
+	public void setDatum(String datum) {
+		this.datum = datum;
+	}
+	
+	public Ocena(Student student, Predmet predmet, int ocena, String datum) {
+		super();
+		this.student = student;
+		this.predmet = predmet;
+		this.ocena = ocena;
+		this.datum = datum;
+	}
+	
 }
