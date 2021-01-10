e5a31643c74ee63ae58fa29d297ae073f805c242
Tara Pogancev <pogancev.ra80.2018@uns.ac.rs>
2021-01-10 14:41:43 +0100

    #menu_bar Izmena kod akcije zatvaranja profesora koja podrzava serilizaciju

--- src/listeners/action/CloseAppAction.java
+++ src/listeners/action/CloseAppAction.java
@@ -8,6 +8,7 @@ package listeners.action;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.io.File;
+import java.io.IOException;
 
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
@@ -15,6 +16,9 @@ import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
 
 import gui.MainFrame;
+import model.BazaPredmeta;
+import model.BazaProfesora;
+import model.BazaStudenata;
 
 public class CloseAppAction extends AbstractAction {
 
@@ -36,13 +40,45 @@ public class CloseAppAction extends AbstractAction {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		
-		Object[] choices = {"Da", "Ne"};
-		Object defaultChoice = choices[0];
-		
-		int id = JOptionPane.showOptionDialog(MainFrame.getInstance(), "Da li ste sigurni da želite da zatvorite aplikaciju?", "Zatvaranje aplikacije",
-				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, choices, defaultChoice);
-		if (id == JOptionPane.YES_OPTION)
+//		Object[] choices = {"Da", "Ne"};
+//		Object defaultChoice = choices[0];
+//		
+//		int id = JOptionPane.showOptionDialog(MainFrame.getInstance(), "Da li ste sigurni da želite da zatvorite aplikaciju?", "Zatvaranje aplikacije",
+//				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, choices, defaultChoice);
+//		if (id == JOptionPane.YES_OPTION)
+//			System.exit(0);
+
+		Object[] opcije = { "Da", "Ne" };
+		Object defaultChoice = opcije[0];
+
+		int izbor = JOptionPane.showOptionDialog(MainFrame.getInstance(),
+				"Da li ste sigurni da želite da zatvorite aplikaciju?", "Zatvaranje aplikacije",
+				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcije, defaultChoice);
+
+		if (izbor == JOptionPane.YES_OPTION) {
+
+			int serijalizacija = JOptionPane.showOptionDialog(MainFrame.getInstance(),
+					"Da li želite da sačuvate izmenjene podatke?", "Čuvanje podataka", JOptionPane.YES_NO_OPTION,
+					JOptionPane.QUESTION_MESSAGE, null, opcije, defaultChoice);
+
+			if (serijalizacija == JOptionPane.YES_OPTION) {
+				try {
+
+					BazaStudenata.getInstance().saveDataStudent();
+					BazaProfesora.getInstance().saveDataProfesor();
+					BazaPredmeta.getInstance().saveDataPredmet();
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				JOptionPane.showMessageDialog(MainFrame.getInstance(), "Podaci su uspešno sačuvani!");
+			}
+			
 			System.exit(0);
+			
+		}
+		
 		
 	}
 
